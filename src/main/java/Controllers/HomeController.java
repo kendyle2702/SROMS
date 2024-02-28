6a28c7f2d508b8a0fe9d799f02f8a5befba354d2
kendyle2702 <kendyle2702@gmai.com>
2024-02-28 21:51:15 +0700

    init project

---
+++ src/main/java/Controllers/HomeController.java
@@ -0,0 +1,31 @@
+package Controllers;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+public class HomeController extends HttpServlet {
+   
+   
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+      
+
+       
+    } 
+
+   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        
+    }
+
+}
